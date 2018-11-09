8ac4708d3c60935783e998c271fe1432bfea4d2e
Steven Heffner <stevenheffner@Stevens-MacBook-Pro-2.local>
2018-11-09 09:48:38 -0700

    refactor

--- cuePractice2.js
+++ cuePractice2.js
@@ -13,7 +13,8 @@
 var _sec = 0;
 var routine_stopped = 0;
 var sec_interval = 10; //say a phrase every N seconds.
-var total_duration = 2*60; // (in seconds) 
+var total_duration = 2*60; // (in seconds)
+
 var phrasen = 0;
 var phrases=[
 "This light is my dream guide. It helps me remember that I am inside a dream.",
@@ -23,6 +24,12 @@ var phrases=[
 "Car lights. A light house. A light bulb. Any shiny object may be your sign that you are now dreaming.",
 "When you see this light, you are in full control of your dreams."
 ];
+
+var remsArray=[];
+var fiveMinutes= 30000     //milliseconds
+var oneHour = 1000 * 3600  //milliseconds
+var halfHour = 1000 * 1800 //milliseconds
+
 
 if (false) //set to true for using mode realistic parameters. set to false to test by shortening time values.
 {
@@ -60,7 +67,6 @@ function button2(buttondown)
 	if (buttondown)
 	{
 		ZMax_Toast("Button 2 pressed", "#0099ff"); 
-	 
 	}
 }
 // ***********************************************************
@@ -77,7 +83,40 @@ function button3(buttondown)
 		routine_stopped = 1;
 	}
 }
-function DoStimulation()
+function DoStimulation1()
+{
+	var vibrate = 0;
+	var alternateEyes = 0;
+	var color = "white";
+	var intensity = 100; // 0 to 100
+	var onTime = 2; // * 100ms units
+	var offTime = 4; // * 100ms units
+	var reps = 4;
+	ZMax_FlashLEDS(color, intensity, onTime, offTime, reps, vibrate, alternateEyes); 
+}
+function DoStimulation2()
+{
+	var vibrate = 0;
+	var alternateEyes = 0;
+	var color = "white";
+	var intensity = 100; // 0 to 100
+	var onTime = 2; // * 100ms units
+	var offTime = 4; // * 100ms units
+	var reps = 4;
+	ZMax_FlashLEDS(color, intensity, onTime, offTime, reps, vibrate, alternateEyes); 
+}
+function DoStimulation3()
+{
+	var vibrate = 0;
+	var alternateEyes = 0;
+	var color = "white";
+	var intensity = 100; // 0 to 100
+	var onTime = 2; // * 100ms units
+	var offTime = 4; // * 100ms units
+	var reps = 4;
+	ZMax_FlashLEDS(color, intensity, onTime, offTime, reps, vibrate, alternateEyes); 
+}
+function DoStimulation4()
 {
 	var vibrate = 0;
 	var alternateEyes = 0;
@@ -109,10 +148,53 @@ function secondTimer(bodytemp, lightlevel, batteryvoltage, BPM, dx, dy, dz, nasa
 			{
 				phrasen = 0;
 			}
-			DoStimulation();
+			DoStimulation1();
 		}
 	}
 	_sec = _sec+1; 
+}
+
+var mp3Path = "C:\\mp3\\mornigmood.mp3"; ////path to mp3 file on your computer
+var mp3Volume = 0.5;                     ////sets volume [0 to 1]
+
+function playMp3(stimulation){
+	ZMax_StopSound()
+  ZMax_PlaySound(mp3Path, 0)
+	ZMax_SetSoundVolume(mp3Volume)
+	if (stimulation)
+	{
+		stimulation()
+	}
+}
+
+var phrasesSeconds = 0
+
+function playPhrases(stimulation){
+
+	clearInterval()
+	phrasesSeconds = 0
+
+	setInterval(function(){
+		phrasesSeconds++
+	}, 1000)
+
+	if (phrasesSeconds % sec_interval == 0)
+	{
+	  useVolume = (total_duration-phrasesSeconds) / total_duration;
+	  ZMax_SetSpeechVolume(Math.floor(useVolume*100));
+	  ZMax_SetSpeechRate(-2);
+	  phrase = phrases[phrasen];
+	  ZMax_SpeakAsync(phrase); 
+	  phrasen = phrasen + 1;
+	  if (phrasen > phrases.length - 1)
+    {
+	  	phrasen = 0;
+	  }
+	  if(stimulation)
+	  {
+	   stimulation();
+	  }
+  }
 }
 
 // ***********************************************************
@@ -120,11 +202,49 @@ function secondTimer(bodytemp, lightlevel, batteryvoltage, BPM, dx, dy, dz, nasa
 // ----------------------------------------------------------
 // called every 256*30 samples of data received (30 seconds)
 // ***********************************************************
-function NewEpochReceived(nEpoch, dxyz, isREM)
+
+function NewEpochReceived(isREM)
 {    
-	if (isREM==true)
+	if (isREM)
 	{
-			DoStimulation();
+		remsArray.push(new Date().getTime());
+		var totalRems = remsArray.length;
+		var timeSinceLastRem = !remsArray[0] ? remsArray[totalRems - 1] - remsArray[totalRems - 2] : 0		
+		if (totalRems == 1 && timeSinceLastRem < fiveMinutes)
+		{
+			DoStimulation1()
+			// playMp3(DoStimulation1)
+			// playPhrases(DoStimulation1)
+		}
+		else if (totalRems == 2 && timeSinceLastRem < fiveMinutes)
+		{
+			DoStimulation2()
+			// playMp3(DoStimulation2)
+			// playPhrases(DoStimulation2)
+		}
+		else if (totalRems == 3 && timeSinceLastRem < fiveMinutes)
+		{
+			DoStimulation3()
+			// playMp3(DoStimulation3)
+			// playPhrases(DoStimulation3)
+		}
+		else if (totalRems == 4 && timeSinceLastRem < fiveMinutes)
+		{
+			DoStimulation4()
+			// playMp3(DoStimulation4)
+			// playPhrases(DoStimulation4)
+		}
+		else if (totalRems > 0 && timeSinceLastRem > oneHour)
+		{
+			DoStimulation2()
+			// playMp3(DoStimulation4)
+			// playPhrases(DoStimulation4)
+		}
+		else 
+		{
+			// Default DoStimulation
+			DoStimulation1()
+		}
 	}
 }
 
